db5642298433fbdb6dd7ad81b66d111c513a276d
Roberto <roberco@hotmail.com>
2020-05-13 17:59:20 +0200

    test update

--- src/tokensTests/NumberTokenTest.java
+++ src/tokensTests/NumberTokenTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import tokens.NumberToken;
+import tokens.WordToken;
 
 class NumberTokenTest {
 
@@ -14,6 +15,14 @@ class NumberTokenTest {
 		assertEquals("3",new NumberToken(3.0).toString());
 		assertEquals("0.03",new NumberToken(0.03).toString());
 		assertEquals("0.000365",new NumberToken(0.000365).toString());
+		
+		NumberToken n = new NumberToken(3.00);
+		NumberToken nb= new NumberToken(4.);
+		WordToken w = new WordToken("ok");
+		assertFalse(n.equals(w));
+		assertFalse(n.equals(nb));
+		NumberToken nbr = new NumberToken(4.00000);
+		assertTrue(nb.equals(nbr));
 	}
 
 }
