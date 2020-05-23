60412083470f0ee8a4bc8bc99559067567b2049f
Roberto <roberco@hotmail.com>
2020-05-23 14:03:31 +0200

    update

--- src/tokenizer/StreamTokenizerTest.java
+++ src/tokenizer/StreamTokenizerTest.java
@@ -14,7 +14,7 @@ class StreamTokenizerTest {
 	@Test
 	void test() throws IOException {
 		StreamTokenizer streamTokenizer = new StreamTokenizer(
-		        new StringReader("Mary had little lambs \n 1 ."));
+		        new StringReader("Mary had little lambs \n 1 "));
 		List<String> tokens = new ArrayList<String>();
 		assertEquals(streamTokenizer.TT_EOF,-1);
 		assertEquals(streamTokenizer.TT_EOL,'\n');
@@ -27,7 +27,7 @@ class StreamTokenizerTest {
 		        System.out.println(streamTokenizer.sval);
 		        tokens.add(streamTokenizer.sval);
 		    } else if(streamTokenizer.ttype == StreamTokenizer.TT_NUMBER) {
-		        System.out.print(streamTokenizer.nval);
+		        System.out.println(streamTokenizer.nval);
 		    	assertEquals(streamTokenizer.lineno(),2.);
 		    	tokens.add(streamTokenizer.nval+"a");
 		    } else if(streamTokenizer.ttype == StreamTokenizer.TT_EOL) {
@@ -42,6 +42,19 @@ class StreamTokenizerTest {
 		tokens.remove("Mary");
 		assertEquals(tokens.size(), 4);
 		assertFalse(tokens.contains("Mary"));
+		
+		String s = "*";
+		//System.out.println(s.getBytes()[0]); // "*" en bytes = 42
+		StreamTokenizer st= new StreamTokenizer(new StringReader("i have gotten much better at that *worth a shot!"));
+		st.commentChar(42); 
+		List<String> t = new ArrayList<String>();
+
+		while(st.nextToken() != StreamTokenizer.TT_EOF){
+		    if(st.ttype == StreamTokenizer.TT_WORD) 
+		      t.add(streamTokenizer.sval);	    
+		    }
+		assertEquals(t.size(), 7);
+		
 		
 	
 	}
