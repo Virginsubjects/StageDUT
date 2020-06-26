de7472041639e2000841af13f405aa87c6122db7
Roberto <roberco@hotmail.com>
2020-06-26 11:39:01 +0200

    Revert "Adding some errors messages which can appear in display from GUI"
    This reverts commit 0a9671d961238760e789e44d4b24f93f70230114.

--- src/concernSlicer/ConcernSlicer.java
+++ src/concernSlicer/ConcernSlicer.java
@@ -11,7 +11,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.text.NumberFormat;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
@@ -31,9 +30,6 @@ public class ConcernSlicer {
 	private static boolean trace = false;
 	private static final String CONCERN = "concerns\\";
 	private static final String COLORIZED = "colorized.html";
-	private static final String[] colors = { "black","navy","blue","green","teal","lime","aqua","maroon",
-											"purple","olive", "gray","silver","red","fuchsia","yellow","white"};
-	private static String message = "";
 	
 	static {
 		NumberFormat nf = NumberFormat.getInstance(Locale.US);
@@ -48,21 +44,22 @@ public class ConcernSlicer {
 			"<title>" + title + " </title>" + nl +
 			"</head>" 			+ nl +
 			"<body>" 			+ nl +
-			"<pre><code>";
+			"<pre>";
 	}
 	
 	private static String getHTMLEnder() {
-		return 	"</code></pre>" + nl +
+		return 	"</pre>" + nl +
 				"</body>" 		+ nl +
 				"</html>" 		+ nl;
 	}
 	
-	public static String colorize (File file, ArrayList<String> list) throws IOException {
+	public static void colorize (File file, ArrayList<String> list) throws IOException {
 		
 		String filename = file.getAbsolutePath();
 		String codeDir = file.getParentFile().getAbsolutePath()+"\\";
 		String colorized = codeDir + COLORIZED;
 		String concernsDir = codeDir + CONCERN;		
+		int i = 0;
 		for (String s : list) {			
 			Path ipath = Paths.get(concernsDir + s);
 			addConcern(s, ipath);
@@ -70,36 +67,21 @@ public class ConcernSlicer {
 		Path codePath = Paths.get(filename);
 		Path colorPath = Paths.get(colorized);		
 		slice(codePath, colorPath);
-		return message;
 	}
 		
 	private static void slice(Path codePath, Path colorPath) throws IOException {
 		List<IToken> tokens = tokenize(codePath,false);
 		colorize(tokens, colorPath);
 	}
-	
-	private static boolean contains(String color) {
-		boolean trouv = false;
-		for(int i = 0 ; i < colors.length ; i++) {
-			if(color.equals(colors[i])) {
-				trouv = true;
-			break;
-			}
-		}		
-		return trouv;		
-	}	
-	
+
 	private static void addConcern(String name, Path ipath) throws IOException {
-		List<IToken> tokens = tokenize(ipath, true);	
-		
-		if(!contains(tokens.get(0).getString()))
-			message += nl+"Mauvais choix de couleur dans le fichier : "+ipath.getFileName().toString()+
-					", le "+tokens.get(0).getString()+" ne fait pas partie des couleurs autorisées";
+		List<IToken> tokens = tokenize(ipath, true);
 		if (trace) {
 			//System.out.println("Added concern "+name+ " " + color+ " "+ ipath);
-			;
+			for(IToken token : tokens)
+				System.out.println(token);
 		}
-		concerns.add(new Concern(name, tokens));				
+		concerns.add(new Concern(name, tokens));
 	}
 	
 	public static List<IToken> detectConcerns(List<IToken>  tokens, List<Concern> concerns) {
